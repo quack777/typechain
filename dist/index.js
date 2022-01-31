e3ce91c0914b3c3fee1720bc3b829dcb07b2969d
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-01 05:14:24 +0900

    Types in Typescrip

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
-var name = "eunchong", age = 24, gender = "male";
+Object.defineProperty(exports, "__esModule", { value: true });
 var sayHi = function (name, age, gender) {
-    console.log("Hello ".concat(name, ", you are ").concat(age, ", you are ").concat(gender, "!"));
+    return "Hello ".concat(name, ", you are ").concat(age, ", you are ").concat(gender, "!!");
 };
-sayHi(name, age, gender);
+console.log(sayHi("eunchong", 23, "male"));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are ${gender}!!`;
+};
+
+console.log(sayHi("eunchong", 23, "male"));
+
+export {};
