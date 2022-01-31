3a806acb8da5cc15d492deef0f65938f1a0dbcc7
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-01 05:35:41 +0900

    Classes on Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var person = {
-    name: "eunchong",
-    age: 23,
-    gender: "male",
-};
+var Human = /** @class */ (function () {
+    function Human(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    return Human;
+}());
+var eunchong = new Human("eunchong", 23, "male");
 var sayHi = function (person) {
     return "Hello ".concat(person.name, ", you are ").concat(person.age, ", you are ").concat(person.gender, "!!");
 };
-console.log(sayHi(person));
+console.log(sayHi(eunchong));
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+import { hrtime } from "process";
+
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "eunchong",
-  age: 23,
-  gender: "male",
-};
+const eunchong = new Human("eunchong", 23, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(eunchong));
 
 export {};
